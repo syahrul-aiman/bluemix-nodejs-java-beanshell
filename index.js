8358e85e705d0a61db6c6e336b0b0c7a1412aa0e
psyntium <psyntium@hotmail.com>
2016-03-18 23:25:39 +0800

    minor update

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var fs = require('fs');
 var hogan = require('hogan-express');
-var bodyParser = require('body-parser')
+var bodyParser = require('body-parser');
+var childProcess = require('child_process');
 
-const exec = require('child_process').exec;
+const exec = childProcess.exec;
 
 var app = express();
 app.use(express.static(__dirname + '/public'));
@@ -16,27 +17,33 @@ app.set('views', __dirname + '/views');
 app.engine('html', hogan);
 app.set('view engine', 'html');
 
-
-app.all('/code', function(req, res) {
+// the code will be stored inside a temporary file, tmp.bsh
+// then the beanshell will intepret the tmp.bsh file
+app.post('/code', function(req, res) {
 	var code = req.body.code;
 	var params = req.body.params;
-	
+
+	if (code == undefined || code == "") {
+		res.json({out: "", err: "Empty command"});
+		return;
+	}	
 	fs.writeFile("tmp.bsh", code, function(err) {
 		if(err) {
 			res.json({out: "", err: err});
+			return;
 		}
 	
 		var cmd = "java -cp lib/bsh-2.0b6.jar " + params + "bsh.Interpreter tmp.bsh";
 		exec(cmd, {shell: '/bin/bash'}, function (err, stdout, stderr) {
 			res.json({out: stdout, err: stderr});
-			return
+			return;
 		});
 	});
 });
 
 app.all(['/','/index'], function (req, res, next) {
 	res.render('index.html');
-})
+});
 
 var port = process.env.PORT || 1337;
 app.listen(port, function() {
